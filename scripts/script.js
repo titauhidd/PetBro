d69dbc39ba5d10172d868cee5e858592fdf2ef4b
titauhidd <tauhidultonmoy1234@gmail.com>
2025-04-29 01:19:53 +0600

    made the sort view buttons with section and the grid layout for pets category

--- scripts/script.js
+++ scripts/script.js
@@ -9,10 +9,10 @@ function displaybuttons(buttons) {
   // console.log(buttons)
   const btndiv = document.getElementById("ctg-btn");
   buttons.forEach((btn) => {
-    console.log(btn.category_icon);
+    // console.log(btn.category_icon);
     const createBtn = document.createElement("div");
     createBtn.innerHTML = `
-    <button class="btn py-10 px-12"><span>
+    <button class="btn py-10 px-12 rounded-lg"><span>
          <img class="w-[80%]" src="${btn.category_icon}" alt=""> 
     </span><span class="text-xl font-bold">${btn.category}s</span></button>
     `;
@@ -21,3 +21,45 @@ function displaybuttons(buttons) {
 }
 
 loadbuttons();
+
+// Show ALl Pets:-----------------
+
+function loadpets() {
+  fetch("https://openapi.programming-hero.com/api/peddy/pets")
+    .then((res) => res.json())
+    .then((data) => displaypets(data.pets));
+}
+function displaypets(pets) {
+  console.log(pets);
+  const petsCategory = document.getElementById("pets-ctg");
+  pets.forEach((petli) => {
+    console.log(petli);
+    const displaypetsCtg = document.createElement("div");
+    displaypetsCtg.classList = "shadow-md p-6 rounded-md";
+    displaypetsCtg.innerHTML = `
+    
+            <div class="">
+              <img class=" rounded-lg" src="./assets/739f0c9c-b653-4a8d-97f7-888895527627.jpeg" alt="">
+              
+            </div>
+            <div class="border-b-[0.5px] border-gray-400" >
+              <p class="pt-2">Mister Tartost</p>
+              <p class="pt-2"><span><i class="fa-solid fa-square"></i></span></p>
+              <p class="pt-2"><span><i class="fa-solid fa-cake-candles"></i></span></p>
+              <p class="pt-2"><span><i class="fa-solid fa-venus"></i></span></p>
+              <p class="pt-2 pb-4"><span><i class="fa-solid fa-dollar-sign"></i></span></p>
+
+            </div>
+            <div class="pt-4 flex items-center justify-between">
+              <button class="btn rounded-md px-6"><span><i class="fa-solid fa-thumbs-up"></i></span></button>
+              <button class="btn rounded-md px-6 text-[#0E7A81]">Adopt</button>
+              <button class="btn rounded-md px-6 text-[#0E7A81]">Details</button>
+            </div>
+
+          
+    `;
+    petsCategory.appendChild(displaypetsCtg);
+  });
+}
+
+loadpets();
